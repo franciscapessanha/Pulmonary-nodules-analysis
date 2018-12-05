1f3948774880ae5e2adfc781d7311502098c66cb
Francisca Pessanha <mariafranciscapessanha@gmail.com>
2018-12-05 11:30:15 +0000

    Update 2D Segmentation

--- 2D_segmentation.py
+++ 2D_segmentation.py
@@ -14,12 +14,6 @@ from sklearn.svm import SVC
 train_slices, train_slices_masks, y_train, test_slices, test_slices_masks, y_test , val_slices, val_slices_masks, y_val = getData()
 
 
-smooth_img = gaussianSmooth(train_slices)
-eigen_nodules = getEigNodules(smooth_img)
-shape_index = getSI (eigen_nodules)
-CV_nodules = getCV(eigen_nodules)
-Vmed_nodules = getVmed(eigen_nodules)
-
 #%%
 samples = [[],[],[],[]]
 labels = []
@@ -52,11 +46,12 @@ def number_points (i_nodules, i_non_nodules):
     return number_of_points_nodules, number_of_points_non_nodules
 
 """      
-def getFeaturePoints(features_n, features_nn):
+def getFeaturePoints(features_n, features_nn, number_of_pixel):
+    
     i_nodules, i_non_nodules = getIndexes(features_n,features_nn)
     
-    i_n = [i_nodules[j] for j in range(10)]
-    i_nn = [i_non_nodules[j] for j in range(10)]
+    i_n = [i_nodules[j] for j in range(number_of_pixel)]
+    i_nn = [i_non_nodules[j] for j in range(number_of_pixel)]
     
     nodule_points = [features_n[:,i] for i in i_n]
     non_nodule_points = [features_nn[:,i] for i in i_nn]
@@ -64,23 +59,99 @@ def getFeaturePoints(features_n, features_nn):
     points = np.concatenate((nodule_points, non_nodule_points), axis = 0)
     labels = np.concatenate((np.ones(np.shape(nodule_points)[0]),np.zeros(np.shape(non_nodule_points)[0])), axis = 0)
     return points, labels
+
+def confusion_matrix(predictions, labels):
+    true_positives = 0
+    false_negatives = 0
+    false_positives = 0
+    true_negatives = 0
     
-for i in range(len(train_slices)):
-    sample_features = [train_slices[i], shape_index[i], CV_nodules[i], Vmed_nodules[i]]
-    sample = train_slices[i]
-    mask = train_slices_masks[i]
+    for i in range(len(predictions)):
+        if predictions[i] == labels[i] :
+            if  predictions[i] == 1.0:
+                true_positives += 1
+            elif  predictions[i] == 0.0:
+                true_negatives += 1
+        elif predictions[i] != labels[i]:
+            if predictions[i] == 1.0:
+                false_positives += 1
+            elif predictions[i] == 0.0:
+                false_negatives += 1
     
-    features_n, features_nn = separateFeatures(sample_features, sample, mask)
+    return np.asarray([[true_positives, false_negatives], [false_positives, true_negatives]])
+
+
+def mainSegmentation(train_slices, train_slices_masks, number_of_pixel):
     
-    if i == 0:
-        points, labels = getFeaturePoints(features_n, features_nn)
-    else:
-        p, l = getFeaturePoints(features_n, features_nn)
-        points = np.concatenate((points, p), axis = 0)
-        labels = np.concatenate((labels, l), axis = 0)
+    smooth_img = gaussianSmooth(train_slices)
+    eigen_nodules = getEigNodules(smooth_img)
+    shape_index = getSI (eigen_nodules)
+    CV_nodules = getCV(eigen_nodules)
+    Vmed_nodules = getVmed(eigen_nodules)
+    
+    for i in range(len(train_slices)):
+        sample_features = [train_slices[i], shape_index[i], CV_nodules[i], Vmed_nodules[i]]
+        sample = train_slices[i]
+        mask = train_slices_masks[i]
+    
+        features_n, features_nn = separateFeatures(sample_features, sample, mask)
+    
+        if i == 0:
+            points, labels = getFeaturePoints(features_n, features_nn, number_of_pixel)
+        else:
+            p, l = getFeaturePoints(features_n, features_nn, number_of_pixel)
+            points = np.concatenate((points, p), axis = 0)
+            labels = np.concatenate((labels, l), axis = 0)
+            
+    return points, labels
     
 
 #%%
-model = SVC(gamma='scale', decision_function_shape='ovo', class_weight='balanced')
+
+points,labels = mainSegmentation(train_slices, train_slices_masks, 10)
+
+model = SVC(decision_function_shape='ovo', class_weight='balanced')
 model.fit(points,labels)
-model.predict()
+
+smooth_img = gaussianSmooth(val_slices)
+eigen_nodules = getEigNodules(smooth_img)
+shape_index = getSI (eigen_nodules)
+CV_nodules = getCV(eigen_nodules)
+Vmed_nodules = getVmed(eigen_nodules)
+
+val_new_slices = []
+shape_index_new_mask = []
+CV_mask_new = []
+Vmed_mask_new = []
+val_masks_new_slices = []
+
+for i in range(len(shape_index)):
+    lung_mask = get_lung_mask(val_slices[i])
+    val_new_slices.append(val_slices[i][lung_mask==1])
+    shape_index_new_mask.append(shape_index[i][lung_mask==1])
+    CV_mask_new.append(CV_nodules[i][lung_mask==1])
+    Vmed_mask_new.append(Vmed_nodules[i][lung_mask==1])
+    val_masks_new_slices.append(val_slices_masks[i][lung_mask==1])
+    
+for i in(range(len(shape_index_new_mask))):
+    if i == 0:
+        val_slices_px = val_new_slices[i]
+        si_px = shape_index_new_mask[i]
+        cv_px = CV_mask_new[i]
+        Vmed_px = Vmed_mask_new[i]
+        Mask_px = val_masks_new_slices[i]
+    else:
+        val_slices_px= np.concatenate((val_slices_px, val_new_slices[i]), axis = 0)
+        si_px = np.concatenate((si_px, shape_index_new_mask[i]), axis = 0)
+        cv_px = np.concatenate((cv_px , CV_mask_new[i]), axis = 0)
+        Vmed_px = np.concatenate((Vmed_px, Vmed_mask_new[i]), axis = 0)
+        Mask_px = np.concatenate((Mask_px,val_masks_new_slices[i]), axis = 0)
+        
+
+val_set = np.asarray((val_slices_px, si_px, cv_px, Vmed_px))
+val_set = np.transpose(val_set)
+
+predictions = model.predict(val_set)
+
+conf_matrix = confusion_matrix(predictions, Mask_px)
+#print(conf_matrix)
