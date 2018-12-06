cc0edaa062426c25cb2cd532084d551f178660b9
Francisca Pessanha <mariafranciscapessanha@gmail.com>
2018-12-06 13:56:11 +0000

    Implement cross-validation mode

--- 2D_segmentation.py
+++ 2D_segmentation.py
@@ -10,6 +10,7 @@ import numpy as np
 from lung_mask import get_lung_mask
 from hessian_based import getEigNodules, gaussianSmooth, getSI, getCV, getVmed
 from sklearn.svm import SVC
+from sklearn.model_selection import cross_val_score
 
 train_slices, train_slices_masks, y_train, test_slices, test_slices_masks, y_test , val_slices, val_slices_masks, y_val = getData()
 
@@ -38,14 +39,7 @@ def getIndexes(features_n, features_nn):
     np.random.shuffle(i_non_nodules)
     return i_nodules, i_non_nodules
 
-"""
-def number_points (i_nodules, i_non_nodules):
-    #definir o numero de pontos que vamos tirar de cada nodulo e de cada nao nodulo de uma imagem
-    number_of_points_nodules = int(round(len(i_nodules)*0.2))
-    number_of_points_non_nodules = int(round(len(i_non_nodules)*0.2))
-    return number_of_points_nodules, number_of_points_non_nodules
-
-"""      
+      
 def getFeaturePoints(features_n, features_nn, number_of_pixel):
     
     i_nodules, i_non_nodules = getIndexes(features_n,features_nn)
@@ -60,26 +54,41 @@ def getFeaturePoints(features_n, features_nn, number_of_pixel):
     labels = np.concatenate((np.ones(np.shape(nodule_points)[0]),np.zeros(np.shape(non_nodule_points)[0])), axis = 0)
     return points, labels
 
-def confusion_matrix(predictions, labels):
-    true_positives = 0
-    false_negatives = 0
-    false_positives = 0
-    true_negatives = 0
+def mask_gt(train_slices, train_slices_masks):
+    #aplicar a mascara do pulmao as imagens 
+    lung_px=[]
+    gt_px=[]
+    t_p=[]
+    f_n=[]
     
-    for i in range(len(predictions)):
-        if predictions[i] == labels[i] :
-            if  predictions[i] == 1.0:
-                true_positives += 1
-            elif  predictions[i] == 0.0:
-                true_negatives += 1
-        elif predictions[i] != labels[i]:
-            if predictions[i] == 1.0:
-                false_positives += 1
-            elif predictions[i] == 0.0:
-                false_negatives += 1
+    for i in range(len(train_slices)):
+        t_positive = 0
+        f_negative = 0
+        lung_mask = get_lung_mask(train_slices[i])
+        gt_px.append(train_slices_masks[i][lung_mask==0])
+        
+        gt_new_px=[]
+        
+        if i == 0:
+            gt_new_px = gt_px[i]
+        else:
+            gt_new_px = np.concatenate((gt_new_px, gt_px[i]), axis = 0)
+        
+        for j in range(len(gt_new_px)):
+                        
+            if gt_new_px[j]==1:
+                f_negative = f_negative+1
+            else:
+                t_positive = t_positive+1
+                
+        t_p.append(t_positive)  
+        f_n.append(f_negative)
+        
+        tp = sum(t_p)
+        fn = sum(f_n)
+        
+    return(tp,fn)
     
-    return np.asarray([[true_positives, false_negatives], [false_positives, true_negatives]])
-
 
 def mainSegmentation(train_slices, train_slices_masks, number_of_pixel):
     
@@ -104,54 +113,103 @@ def mainSegmentation(train_slices, train_slices_masks, number_of_pixel):
             labels = np.concatenate((labels, l), axis = 0)
             
     return points, labels
+
+def input_set(val_slices, val_slices_masks):
+
+    smooth_img = gaussianSmooth(val_slices)
+    eigen_nodules = getEigNodules(smooth_img)
+    shape_index = getSI (eigen_nodules)
+    CV_nodules = getCV(eigen_nodules)
+    Vmed_nodules = getVmed(eigen_nodules)
     
+    val_new_slices = []
+    shape_index_new_mask = []
+    CV_mask_new = []
+    Vmed_mask_new = []
+    val_masks_new_slices = []
+    
+    for i in range(len(shape_index)):
+        lung_mask = get_lung_mask(val_slices[i])
+        val_new_slices.append(val_slices[i][lung_mask==1])
+        shape_index_new_mask.append(shape_index[i][lung_mask==1])
+        CV_mask_new.append(CV_nodules[i][lung_mask==1])
+        Vmed_mask_new.append(Vmed_nodules[i][lung_mask==1])
+        val_masks_new_slices.append(val_slices_masks[i][lung_mask==1])
+        
+    for i in(range(len(shape_index_new_mask))):
+        if i == 0:
+            val_slices_px = val_new_slices[i]
+            si_px = shape_index_new_mask[i]
+            cv_px = CV_mask_new[i]
+            Vmed_px = Vmed_mask_new[i]
+            mask_px = val_masks_new_slices[i]
+        else:
+            val_slices_px= np.concatenate((val_slices_px, val_new_slices[i]), axis = 0)
+            si_px = np.concatenate((si_px, shape_index_new_mask[i]), axis = 0)
+            cv_px = np.concatenate((cv_px , CV_mask_new[i]), axis = 0)
+            Vmed_px = np.concatenate((Vmed_px, Vmed_mask_new[i]), axis = 0)
+            mask_px = np.concatenate((mask_px,val_masks_new_slices[i]), axis = 0)
+        
+
+    val_set = np.asarray((val_slices_px, si_px, cv_px, Vmed_px))
+    val_set = np.transpose(val_set)
+    
+    return(val_set, mask_px)
+   
+def confusion_matrix(predictions, labels, val_slices, val_slices_masks):
+    true_positives = 0
+    false_negatives = 0
+    false_positives = 0
+    true_negatives = 0
+    
+    for i in range(len(predictions)):
+        if predictions[i] == labels[i] :
+            if  predictions[i] == 1.0:
+                true_positives += 1
+            elif  predictions[i] == 0.0:
+                true_negatives += 1
+        elif predictions[i] != labels[i]:
+            if predictions[i] == 1.0:
+                false_positives += 1
+            elif predictions[i] == 0.0:
+                false_negatives += 1
+                
+    tp, fn = mask_gt(val_slices, val_slices_masks)
+    
+    true_positives = true_positives + tp
+    false_negatives = false_negatives + fn
+    
+    dice = (2*true_positives/(false_positives+false_negatives+(2*true_positives)))
+    
+    jaccard = (true_positives)/(true_positives+false_positives+false_negatives)
+    
+    matrix = np.asarray([[true_positives, false_negatives], [false_positives, true_negatives]])
+    
+    return dice, jaccard, matrix
+
 
 #%%
 
-points,labels = mainSegmentation(train_slices, train_slices_masks, 10)
+points, labels = mainSegmentation(train_slices, train_slices_masks, 10)
 
 model = SVC(decision_function_shape='ovo', class_weight='balanced')
 model.fit(points,labels)
-
-smooth_img = gaussianSmooth(val_slices)
-eigen_nodules = getEigNodules(smooth_img)
-shape_index = getSI (eigen_nodules)
-CV_nodules = getCV(eigen_nodules)
-Vmed_nodules = getVmed(eigen_nodules)
-
-val_new_slices = []
-shape_index_new_mask = []
-CV_mask_new = []
-Vmed_mask_new = []
-val_masks_new_slices = []
-
-for i in range(len(shape_index)):
-    lung_mask = get_lung_mask(val_slices[i])
-    val_new_slices.append(val_slices[i][lung_mask==1])
-    shape_index_new_mask.append(shape_index[i][lung_mask==1])
-    CV_mask_new.append(CV_nodules[i][lung_mask==1])
-    Vmed_mask_new.append(Vmed_nodules[i][lung_mask==1])
-    val_masks_new_slices.append(val_slices_masks[i][lung_mask==1])
     
-for i in(range(len(shape_index_new_mask))):
-    if i == 0:
-        val_slices_px = val_new_slices[i]
-        si_px = shape_index_new_mask[i]
-        cv_px = CV_mask_new[i]
-        Vmed_px = Vmed_mask_new[i]
-        Mask_px = val_masks_new_slices[i]
-    else:
-        val_slices_px= np.concatenate((val_slices_px, val_new_slices[i]), axis = 0)
-        si_px = np.concatenate((si_px, shape_index_new_mask[i]), axis = 0)
-        cv_px = np.concatenate((cv_px , CV_mask_new[i]), axis = 0)
-        Vmed_px = np.concatenate((Vmed_px, Vmed_mask_new[i]), axis = 0)
-        Mask_px = np.concatenate((Mask_px,val_masks_new_slices[i]), axis = 0)
-        
-
-val_set = np.asarray((val_slices_px, si_px, cv_px, Vmed_px))
-val_set = np.transpose(val_set)
+val_set, mask_px = input_set(test_slices, test_slices_masks)
 
 predictions = model.predict(val_set)
 
-conf_matrix = confusion_matrix(predictions, Mask_px)
-#print(conf_matrix)
+dice, jaccard, conf_matrix = confusion_matrix(predictions, mask_px, test_slices, test_slices_masks)
+print(conf_matrix, dice, jaccard)
+
+
+
+data = train_slices+val_slices
+labels = train_slices_masks + val_slices_masks
+
+cross_set, cross_mask = input_set(data, labels)
+
+#new_shape_data = np.reshape(data, (5814,51))
+#new_shape_labels = np.reshape(labels, (5814,51))
+
+scores = cross_val_score(model, cross_set, cross_mask, cv=2)
--- get_data.py
+++ get_data.py
@@ -9,19 +9,19 @@ import numpy as np
 import os
 import pandas as pd
 from sklearn.model_selection import train_test_split
-
+from sklearn.model_selection import KFold
 
 """
     Masks e Imagens - fatia do meio (todos os sets)
     
 """
-def getData():
+def getData(mode = "default"):
    nodules, masks, metadata = loadData()
-   x_train, y_train, masks_train, x_test, y_test, masks_test, x_val, y_val, masks_val = getSets(nodules, metadata, masks) 
-   train_slices, train_slices_masks, test_slices, test_slices_masks, val_slices, val_slices_masks = getImages(x_train, masks_train, x_test, masks_test, x_val, masks_val)
+   x_train, y_train, masks_train, x_test, y_test, masks_test, x_val, y_val, masks_val = getSets(nodules, metadata, masks, mode) 
+   train_slices, train_slices_masks, test_slices, test_slices_masks, val_slices, val_slices_masks = getImages(x_train, masks_train, x_test, masks_test, x_val, masks_val, mode )
    
    return train_slices, train_slices_masks, y_train, test_slices, test_slices_masks, y_test , val_slices, val_slices_masks, y_val
-
+   
 
 """
 Find Extensions
@@ -156,7 +156,7 @@ labels:
     non_solid = 0
 """
 
-def getSets(nodules, metadata, masks):
+def getSets(nodules, metadata, masks, mode):
     
     ns_nodules, ss_nodules, s_nodules = splitTextures(nodules, metadata)
     
@@ -164,22 +164,88 @@ def getSets(nodules, metadata, masks):
     ss_train, ss_test, ss_val = splitData(ss_nodules, [1 for i in range(len(ss_nodules))])
     ns_train, ns_test, ns_val = splitData(ns_nodules,[0 for i in range(len(ns_nodules))])
     
-    
-    x_train = np.concatenate((ns_train[0], ss_train[0], s_train[0]), axis = 0)
-    y_train = np.concatenate((ns_train[1], ss_train[1], s_train[1]), axis = 0)
-    
     x_test = np.concatenate((ns_test[0], ss_test[0], s_test[0]), axis = 0)
     y_test = np.concatenate((ns_test[1], ss_test[1], s_test[1]), axis = 0)
-    
-    x_val = np.concatenate((ns_val[0], ss_val[0], s_val[0]), axis = 0)
-    y_val = np.concatenate((ns_val[1], ss_val[1], s_val[1]), axis = 0)
-
-    masks_train = assignMasks(x_train, masks)
     masks_test = assignMasks(x_test, masks)
-    masks_val = assignMasks(x_val, masks) 
+    
+    if mode == "default":
+        x_train = np.concatenate((ns_train[0], ss_train[0], s_train[0]), axis = 0)
+        y_train = np.concatenate((ns_train[1], ss_train[1], s_train[1]), axis = 0)
+        
+        x_val = np.concatenate((ns_val[0], ss_val[0], s_val[0]), axis = 0)
+        y_val = np.concatenate((ns_val[1], ss_val[1], s_val[1]), axis = 0)
+    
+        masks_train = assignMasks(x_train, masks)
+        masks_val = assignMasks(x_val, masks) 
 
+    elif mode == "cross_val":
+        x_ns = np.concatenate((s_train[0],s_val[0]), axis = 0)
+        y_ns = np.concatenate((s_train[1],s_val[1]), axis = 0)
+        
+        x_ss = np.concatenate((ss_train[0],ss_val[0]), axis = 0)
+        y_ss = np.concatenate((ss_train[1],ss_val[1]), axis = 0)
+        
+        x_s = np.concatenate((s_train[0],s_val[0]), axis = 0)
+        y_s = np.concatenate((s_train[1],s_val[1]), axis = 0)
+        
+        n_splits = 10
+       
+        all_x_train_ns = []
+        all_x_val_ns = []
+        all_y_train_ns = []
+        all_y_val_ns = []
+        
+        ns_kf = KFold(n_splits, shuffle = True)
+        for train_index, val_index in ns_kf.split(x_ns):
+            x_train_ns, x_val_ns = x_ns[train_index], x_ns[val_index]
+            y_train_ns, y_val_ns = y_ns[train_index], y_ns[val_index]
+            all_x_train_ns.append(x_train_ns)
+            all_x_val_ns.append(x_val_ns)
+            all_y_train_ns.append(y_train_ns)
+            all_y_val_ns.append(y_val_ns)
+            
+        all_x_train_ss = []
+        all_x_val_ss = []
+        all_y_train_ss = []
+        all_y_val_ss = []
+
+        ss_kf = KFold(n_splits, shuffle = True)
+        for train_index, val_index in ss_kf.split(x_ss):
+            x_train_ss, x_val_ss = x_ss[train_index], x_ss[val_index]
+            y_train_ss, y_val_ss = y_ss[train_index], y_ss[val_index]
+            all_x_train_ss.append(x_train_ss)
+            all_x_val_ss.append(x_val_ss)
+            all_y_train_ss.append(y_train_ss)
+            all_y_val_ss.append(y_val_ss)
+        
+        all_x_train_s = []
+        all_x_val_s = []
+        all_y_train_s = []
+        all_y_val_s = []
+        
+        s_kf = KFold(n_splits, shuffle = True)
+        for train_index, val_index in s_kf.split(x_s):
+            x_train_s, x_val_s = x_s[train_index], x_s[val_index]
+            y_train_s, y_val_s = y_s[train_index], y_s[val_index]
+            all_x_train_s.append(x_train_s)
+            all_x_val_s.append(x_val_s)
+            all_y_train_s.append(y_train_s)
+            all_y_val_s.append(y_val_s)
+        
+        x_train = [np.concatenate((all_x_train_ns[i], all_x_train_ss[i], all_x_train_s[i]), axis = 0) for i in range(n_splits)]
+        y_train = [np.concatenate((all_y_train_ns[i], all_y_train_ss[i], all_y_train_s[i]), axis = 0) for i in range(n_splits)]
+        
+        x_val = [np.concatenate((all_x_val_ns[i], all_x_val_ss[i], all_x_val_s[i]), axis = 0) for i in range(n_splits)]
+        y_val = [np.concatenate((all_y_val_ns[i], all_y_val_ss[i], all_y_val_s[i]), axis = 0) for i in range(n_splits)]
+       
+
+        masks_train = []
+        masks_val = []
+        for i in range(len(x_train)):
+            masks_train.append(assignMasks(x_train[i], masks))
+            masks_val.append(assignMasks(x_val[i], masks))
+        
     return x_train, y_train, masks_train, x_test, y_test, masks_test, x_val, y_val, masks_val
-
 
 """
 Get Middle Slice
@@ -209,26 +275,54 @@ def loadImages(images, masks):
     return new_images, new_masks
 
 
-def getImages(x_train, masks_train, x_test, masks_test, x_val, masks_val):
-    train_nods, train_masks = loadImages(x_train, masks_train)
-    train_slices = []
-    train_slices_masks = []
-    for n in range(len(train_nods)):
-        train_slices.append(getMiddleSlice(train_nods[n]))
-        train_slices_masks.append(getMiddleSlice(train_masks[n]))
-    
+def getImages(x_train, masks_train, x_test, masks_test, x_val, masks_val, mode = "default"):
+            
     test_nods, test_masks = loadImages(x_test, masks_test)
     test_slices = []
     test_slices_masks = []
     for n in range(len(test_nods)):
         test_slices.append(getMiddleSlice(test_nods[n]))
         test_slices_masks.append(getMiddleSlice(test_masks[n]))
-
-    val_nods, val_masks = loadImages(x_val, masks_val)
-    val_slices = []
-    val_slices_masks = []
-    for n in range(len(val_nods)):
-        val_slices.append(getMiddleSlice(val_nods[n]))
-        val_slices_masks.append(getMiddleSlice(val_masks[n]))
-
+    
+    if mode == "default":
+        train_nods, train_masks = loadImages(x_train, masks_train)
+        train_slices = []
+        train_slices_masks = []
+        for n in range(len(train_nods)):
+            train_slices.append(getMiddleSlice(train_nods[n]))
+            train_slices_masks.append(getMiddleSlice(train_masks[n]))
+    
+        val_nods, val_masks = loadImages(x_val, masks_val)
+        val_slices = []
+        val_slices_masks = []
+        for n in range(len(val_nods)):
+            val_slices.append(getMiddleSlice(val_nods[n]))
+            val_slices_masks.append(getMiddleSlice(val_masks[n]))
+        
+    elif mode == "cross_val":
+        train_slices = []
+        train_slices_masks = []
+        val_slices = []
+        val_slices_masks = []
+        for i in range(len(x_train)):
+            train_nods, train_masks = loadImages(x_train[i], masks_train[i])
+            t_slices = []
+            t_slices_masks = []
+            for n in range(len(train_nods)):
+                t_slices.append(getMiddleSlice(train_nods[n]))
+                t_slices_masks.append(getMiddleSlice(train_masks[n]))
+            train_slices.append(t_slices)
+            train_slices_masks.append(t_slices_masks)
+            
+        for i in range(len(x_val)):
+            val_nods, val_masks = loadImages(x_val[i], masks_val[i])
+            t_slices = []
+            t_slices_masks = []
+            for n in range(len(val_nods)):
+                t_slices.append(getMiddleSlice(val_nods[n]))
+                t_slices_masks.append(getMiddleSlice(val_masks[n]))
+            val_slices.append(t_slices)
+            val_slices_masks.append(t_slices_masks)
+              
+          
     return train_slices, train_slices_masks, test_slices, test_slices_masks, val_slices, val_slices_masks
