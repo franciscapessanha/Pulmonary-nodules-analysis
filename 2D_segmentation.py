Correct data normalization

2D_segmentation.py
@@ -60,23 +60,14 @@ def normalizeImages(train_images):
     return mean, std
 
 def getTrainingSet(train_slices, train_slices_masks, number_of_pixel):
-    smooth_img = gaussianSmooth(train_slices)
-    eigen_nodules = getEigNodules(smooth_img)
-    
     mean_int, std_int = normalizeImages(train_slices)
     norm_slices = [(nodule - mean_int)/std_int for nodule in train_slices]
     
+    smooth_img = gaussianSmooth(norm_slices)
+    eigen_nodules = getEigNodules(smooth_img)
     SI_nodules = getSI (eigen_nodules)
-    mean_si, std_si = normalizeImages(SI_nodules)
-    SI_nodules = [(si_nodule - mean_si)/std_si for si_nodule in  SI_nodules]
-    
     CV_nodules = getCV(eigen_nodules)
-    mean_cv, std_cv = normalizeImages(CV_nodules)
-    CV_nodules = [(cv_nodule - mean_cv)/std_cv for cv_nodule in  CV_nodules]
-    
     Vmed_nodules = getVmed(eigen_nodules)
-    mean_vmed, std_vmed = normalizeImages(Vmed_nodules)
-    Vmed_nodules = [(vmed_nodule - mean_vmed)/std_vmed for vmed_nodule in  Vmed_nodules]
     
     for i in range(len(train_slices)):
         sample_features = [norm_slices[i], SI_nodules[i], CV_nodules[i], Vmed_nodules[i]]
@@ -92,20 +83,17 @@ def getTrainingSet(train_slices, train_slices_masks, number_of_pixel):
             points = np.concatenate((points, p), axis = 0)
             labels = np.concatenate((labels, l), axis = 0)
             
-    return points, labels, mean_int, std_int, mean_si, std_si, mean_cv, std_cv, mean_vmed, std_vmed
+    return points, labels, mean_int, std_int
 
-def getInputSet(nodules, masks,mean_int, std_int,  mean_si, std_si, mean_cv, std_cv, mean_vmed, std_vmed):
-    smooth_img = gaussianSmooth(nodules)
+def getInputSet(nodules, masks,mean_int, std_int):
+    norm_nodules = [(nodule - mean_int)/std_int for nodule in nodules]
+    smooth_img = gaussianSmooth(norm_nodules)
     eigen_nodules = getEigNodules(smooth_img)
     SI_nodules = getSI(eigen_nodules)
     CV_nodules = getCV(eigen_nodules)
     Vmed_nodules = getVmed(eigen_nodules)
     
-    norm_nodules = [(nodule - mean_int)/std_int for nodule in nodules]
-    SI_nodules = [(si_nodule - mean_si)/std_si for si_nodule in  SI_nodules]
-    CV_nodules = [(cv_nodule - mean_cv)/std_cv for cv_nodule in  CV_nodules]
-    Vmed_nodules = [(vmed_nodule - mean_vmed)/std_vmed for vmed_nodule in  Vmed_nodules]
-    
+
     masked_nodules = []
     masked_SI = []
     masked_CV = []
@@ -191,8 +179,8 @@ def getPerformanceMetrics(predictions_lung, labels_lung, predictions_outer_lung,
 all_train_slices, all_train_slices_masks, all_y_train, test_slices, test_slices_masks, y_test , all_val_slices, all_val_slices_masks, all_y_val = getData("cross_val")
 for train_slices, train_slices_masks, val_slices, val_slices_masks in zip(all_train_slices, all_train_slices_masks, all_val_slices, all_val_slices_masks):
     print("SVM \n=======")
-    points, labels, mean_int, std_int, mean_si, std_si, mean_cv, std_cv, mean_vmed, std_vmed = getTrainingSet(train_slices, train_slices_masks, 0.15)
-    val_lung, labels_lung  = getInputSet(val_slices, val_slices_masks, mean_int, std_int, mean_si, std_si, mean_cv, std_cv, mean_vmed, std_vmed)
+    points, labels, mean_int, std_int = getTrainingSet(train_slices, train_slices_masks, 0.10 )
+    val_lung, labels_lung  = getInputSet(val_slices, val_slices_masks, mean_int, std_int)
     
     model_SVM = SVC(kernel = 'rbf', random_state = 1,gamma='auto')
     model_SVM.fit(points,labels)
@@ -203,8 +191,8 @@ for train_slices, train_slices_masks, val_slices, val_slices_masks in zip(all_tr
     print("The dice value is %.2f and the jaccard value is %.2f" % (dice, jaccard))
 
 print("SVM test \n=======")
-points, labels, mean_int, std_int, mean_si, std_si, mean_cv, std_cv, mean_vmed, std_vmed = getTrainingSet(train_slices, train_slices_masks, 0.15)
-test_lung, labels_lung  = getInputSet(test_slices, test_slices_masks, mean_int, std_int, mean_si, std_si, mean_cv, std_cv, mean_vmed, std_vmed)
+points, labels, mean_int, std_int= getTrainingSet(train_slices, train_slices_masks, 0.10)
+test_lung, labels_lung  = getInputSet(test_slices, test_slices_masks, mean_int, std_int)
 
 model_SVM = SVC(kernel = 'rbf', random_state = 1,gamma='auto')
 model_SVM.fit(points,labels)
@@ -216,6 +204,11 @@ print("The dice value is %.2f and the jaccard value is %.2f" % (dice, jaccard))
 
 
 """
+reshape(n_features, 51x51) em vez de concatenação!
+==================================================
+Guardar matriz de caracteristicas e apagar nodulos!
+
+
     for i in [9,11,13,15]:
         print("K = %.0f \n=======" % i)
         

texture.py
@@ -47,16 +47,10 @@ def calcIntensityFeatures(nodules, masks):
     
 
 def getIntensityFeatures(train_slices, train_slices_masks, val_slices, val_slices_masks, test_slices, test_slices_masks):
-    
-    mean_int, std_int = normalizeIntensity(train_slices, train_slices_masks)
 
-    train_nodules = [(nodule - mean_int)/std_int for nodule in train_slices]
-    val_nodules = [(nodule - mean_int)/std_int for nodule in val_slices]
-    test_nodules = [(nodule - mean_int)/std_int for nodule in test_slices]
-    
-    train_int_features = np.vstack(calcIntensityFeatures(train_nodules, train_slices_masks))
-    val_int_features = np.vstack(calcIntensityFeatures(val_nodules, val_slices_masks))
-    test_int_features = np.vstack(calcIntensityFeatures(test_nodules, test_slices_masks))
+    train_int_features = np.vstack(calcIntensityFeatures(train_slices, train_slices_masks))
+    val_int_features = np.vstack(calcIntensityFeatures(val_slices, val_slices_masks))
+    test_int_features = np.vstack(calcIntensityFeatures(test_slices, test_slices_masks))
     
     return train_int_features, val_int_features, test_int_features
 
@@ -81,13 +75,6 @@ def getShapeFeatures(train_slices_masks,val_slices_masks, test_slices_masks):
     train_shape_features = np.vstack(calcShapeFeatures(train_slices_masks))
     val_shape_features = np.vstack(calcShapeFeatures(val_slices_masks))
     test_shape_features = np.vstack(calcShapeFeatures(test_slices_masks))
-    
-    mean_shape = np.mean(train_shape_features, axis=0)
-    std_shape = np.std(train_shape_features, axis=0)
-
-    train_shape_features = np.transpose([(train_shape_features[:,i] - mean_shape[i])/std_shape[i] for i in range(len(mean_shape))])
-    val_shape_features = np.transpose([(val_shape_features[:,i] - mean_shape[i])/std_shape[i] for i in range(len(mean_shape))])
-    test_shape_features = np.transpose([(test_shape_features[:,i] - mean_shape[i])/std_shape[i] for i in range(len(mean_shape))])
     
     return train_shape_features, val_shape_features, test_shape_features
   
@@ -118,13 +105,6 @@ def getLBPFeatures(train_slices, train_slices_masks, val_slices, val_slices_mask
     val_lbp = calcLBP(val_slices, val_slices_masks, n_points, radius)
     test_lbp = calcLBP(test_slices, test_slices_masks, n_points, radius)
 
-    mean_lbp = np.mean(np.hstack(train_lbp))
-    std_lbp = np.std(np.hstack(train_lbp))
-    
-    train_lbp = [(value - mean_lbp)/std_lbp for value in train_lbp]
-    val_lbp = [(value - mean_lbp)/std_lbp for value in val_lbp]
-    test_lbp = [(value - mean_lbp)/std_lbp for value in test_lbp]
-    
     max_ = np.max([np.max(np.hstack(train_lbp)), np.max(np.hstack(val_lbp)), np.max(np.hstack(test_lbp))])
     min_ = np.min([np.min(np.hstack(train_lbp)), np.min(np.hstack(val_lbp)), np.min(np.hstack(test_lbp))])
     
@@ -227,8 +207,27 @@ def getPrediction(train_features, y_train, val_features, y_val):
 
 
 #train_slices, train_slices_masks, y_train, test_slices, test_slices_masks, y_test , val_slices, val_slices_masks, y_val = getData()
+
+
+train_int_features, val_int_features, test_int_features = getIntensityFeatures(train_slices, train_slices_masks, val_slices, val_slices_masks, test_slices, test_slices_masks)
+plt.plot(train_int_features[y_train==0,0], 'ro')
+plt.plot(train_int_features[y_train==1,0], 'bo')
+plt.plot(train_int_features[y_train==2,0], 'co')
+
+plt.title("Training set")
+plt.show()
+#plt.scatter(pca_val[:,0], pca_val[:,1], c= y_val)
+#plt.title("Validation set")
+#plt.show()
+
+
 all_train_slices, all_train_slices_masks, all_y_train, test_slices, test_slices_masks, y_test , all_val_slices, all_val_slices_masks, all_y_val = getData("cross_val")
 for train_slices, train_slices_masks, y_train, val_slices, val_slices_masks, y_val in zip(all_train_slices, all_train_slices_masks, all_y_train, all_val_slices, all_val_slices_masks, all_y_val): 
+    
+    mean_int, std_int = normalizeIntensity(train_slices, train_slices_masks)
+    train_slices = (train_slices - mean_int)/std_int
+    val_slices = (val_slices - mean_int)/std_int
+    test_slices = (test_slices - mean_int)/std_int
     
     train_int_features, val_int_features, test_int_features = getIntensityFeatures(train_slices, train_slices_masks, val_slices, val_slices_masks, test_slices, test_slices_masks)
     train_shape_features, val_shape_features, test_shape_features = getShapeFeatures(train_slices_masks, val_slices_masks,test_slices_masks)
@@ -255,5 +254,5 @@ for train_slices, train_slices_masks, y_train, val_slices, val_slices_masks, y_v
     prediction_all = getPrediction(train_features, y_train, val_features, y_val)
     print("Test \n======")
     prediction_all = getPrediction(train_features, y_train, test_features, y_test)
-    
+ 
     